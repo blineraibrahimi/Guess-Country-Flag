da9a3135a37ecdad95aecbb13d8bc9f4389458c2
Blinera Ibrahimi <iblinera@gmail.com>
2021-05-18 09:25:38 +0200

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Blinera Ibrahimi on 18.5.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
